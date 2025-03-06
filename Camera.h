c2664a4aa2a659e6d185da7f63f5d7cc02446b64
IDDQD13Hummingbird <IDDQD13Workshop@gmail.com>
2025-03-06 11:36:14 +0100

    BG

--- Camera.h
+++ Camera.h
@@ -34,6 +34,8 @@ private:
     QMatrix4x4 mViewMatrix{};
 
     QVector3D mPosition{ 0.f, 0.f, 0.f };
+
+    QVector3D mInHousePosition{ 0.f, 0.f, 0.f };
     float mPitch{ 0.f };
     float mYaw{ 0.f };
 
--- WorldAxis.cpp
+++ WorldAxis.cpp
@@ -2,14 +2,24 @@
 
 WorldAxis::WorldAxis()
 {
-	drawType = 1; // 0 = fill, 1 = line
+    //drawType = 1; // 0 = fill, 1 = line
 
-	mVertices.push_back(Vertex{ 0.f, 0.f, 0.f,       1.f, 0.f, 0.f,    0.f, 0.f }); //x-axis
-    mVertices.push_back(Vertex{ 100.f, 0.f, 0.f,     1.f, 0.f, 0.f,    0.f, 0.f });
-	mVertices.push_back(Vertex{ 0.f, 0.f, 0.f,       0.f, 1.f, 0.f,    0.f, 0.f }); //y-axis
-    mVertices.push_back(Vertex{ 0.f, 100.f, 0.f,     0.f, 1.f, 0.f,    0.f, 0.f });
-	mVertices.push_back(Vertex{ 0.f, 0.f, 0.f,       0.f, 0.f, 1.f,    0.f, 0.f }); //z-axis
-    mVertices.push_back(Vertex{ 0.f, 0.f, 100.f,     0.f, 0.f, 1.f,    0.f, 0.f });
+    //mVertices.push_back(Vertex{ 0.f, 0.f, 0.f,       1.f, 0.f, 0.f,    0.f, 0.f }); //x-axis
+    //mVertices.push_back(Vertex{ 100.f, 0.f, 0.f,     1.f, 0.f, 0.f,    0.f, 0.f });
+    //mVertices.push_back(Vertex{ 0.f, 0.f, 0.f,       0.f, 1.f, 0.f,    0.f, 0.f }); //y-axis
+    //mVertices.push_back(Vertex{ 0.f, 100.f, 0.f,     0.f, 1.f, 0.f,    0.f, 0.f });
+    //mVertices.push_back(Vertex{ 0.f, 0.f, 0.f,       0.f, 0.f, 1.f,    0.f, 0.f }); //z-axis
+    //mVertices.push_back(Vertex{ 0.f, 0.f, 100.f,     0.f, 0.f, 1.f,    0.f, 0.f });
+
+    drawType = 0; // 0 = fill, 1 = line
+
+    //x-axis
+    mVertices.push_back(Vertex{ 20.f, -5.f, -5.f,     1.f, 0.75f, 0.75f,    0.f, 0.f });
+    //y-axis
+    mVertices.push_back(Vertex{ 0.f, 20.f, -5.f,     0.75f, 1.f, 0.75f,    0.f, 0.f });
+    //z-axis
+    mVertices.push_back(Vertex{ -20.f, -20.f, -5.f,     0.75f, 0.75f, 1.f,    0.f, 0.f });
+
 
     mMatrix.setToIdentity();
 }
